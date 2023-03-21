ab7fe443af97ada0f51d1c46959966654e2db0ae
Micah Glausier <micah.glausier@me.com>
2023-03-21 14:20:36 -0500

    Updated fatch address

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -1,11 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MovieCard } from "../MovieCard/movie-card";
 import { MovieView } from "../MovieView/movie-view";
 
 export const MainView = () => {
+  
   const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://myflix-micah.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            author: doc.director_name?.[0]
+          };
+        });
+        setMovies(moviesFromApi);
+      });
+  }, []);
+
+  // useEffect(() => {
+  //   fetch("https://myflix-micah.herokuapp.com/movies")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const moviesFromApi = data.docs.map((doc) => {
+  //         return {
+  //           id: doc.key,
+  //           title: doc.title,
+  //           image: doc.image,
+  //           author: doc.director?.[0]
+  //         };
+  //       });
+
+  //       setBooks(booksFromApi);
+  //     });
+  // }, []);
+
+
 
   if (selectedMovie) {
     return (
