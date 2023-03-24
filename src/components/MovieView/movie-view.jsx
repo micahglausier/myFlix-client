280ebe5f9e2fbdab4e58940fef0d8d8a29be6a7f
Micah Glausier <micah.glausier@me.com>
2023-03-23 20:03:09 -0500

    Created .scss for each view

--- src/components/MovieView/movie-view.jsx
+++ src/components/MovieView/movie-view.jsx
@@ -1,3 +1,5 @@
+import './movie-view.scss';
+
 export const MovieView = ({ movie, onBackClick }) => {
     return (
       <div>
@@ -20,7 +22,7 @@ export const MovieView = ({ movie, onBackClick }) => {
           <span>Description: </span>
           <span>{movie.description}</span>
         </div>
-        <button onClick={onBackClick}>Back</button>
+        <button onClick={onBackClick} className="back-button" style={{ cursor: "pointer" }}>Back</button>
       </div>
     );
   };
